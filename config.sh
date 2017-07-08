9413dfa63fb764eed1ba0d26439853c1efa9ffb3
yangxins <yangmame@icloud.com>
2017-07-08 14:09:49 +0800

    Fix

--- config.sh
+++ config.sh
@@ -16,12 +16,12 @@ read -p "Are you efi ? (y or Enter  " TMP
 if (("$TMP"=="y"))
 then TMP=n
 while [ "$TMP" == n ];do
-pacman -S grub efibootmgr -y&&grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=Arch&&grub-mkconfig -o /boot/grub/grub.cfg
+pacman -S --noconfirm grub efibootmgr -y&&grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=Arch&&grub-mkconfig -o /boot/grub/grub.cfg
 read -p "Successfully installed ? (n or Enter  " TMP
 done
 else TMP=n
 while [ "$TMP" == n ];do
-pacman -S grub&&fdisk -l
+pacman -S --noconfirm grub&&fdisk -l
 read -p "Input the disk you want to install the grub  " GRUB
 grub-install --target=i386-pc $GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
@@ -41,7 +41,7 @@ echo "What is your video card ?
 [4]  ATI/AMD"
 read VIDEO
 if (($VIDEO==1))
-then pacman -S xf86-video-intel -y
+then pacman -S --noconfirm xf86-video-intel -y
 elif (($VIDEO==2))
 then TMP=4
 while (($TMP!=1&&$TMP!=2&&$TMP!=3));do
@@ -51,16 +51,16 @@ echo "Version of nvidia-driver to install:
 [3]  Older  "
 read TMP
 if (($TMP==1))
-then pacman -S nvidia -y
+then pacman -S --noconfirm nvidia -y
 elif (($TMP==2))
-then pacman -S nvidia-304xx -y
+then pacman -S --noconfirm nvidia-304xx -y
 elif (($TMP==3))
-then pacman -S nvidia-340xx -y
+then pacman -S --noconfirm nvidia-340xx -y
 else echo error ! input the number again
 fi
 done
 elif (($VIDEO == 3))
-then pacman -S bumblebee -y
+then pacman -S --noconfirm bumblebee -y
 systemctl enable bumblebeed
 TMP=4
 while (($TMP!=1&&$TMP!=2&&$TMP!=3));do
@@ -70,16 +70,16 @@ echo "Version of nvidia-driver to install:
 [3]  Older   " 
 read TMP
 if (($TMP==1))
-then pacman -S nvidia -y
+then pacman -S --noconfirm nvidia -y
 elif (($TMP==2))
-then pacman -S nvidia-304xx -y
+then pacman -S --noconfirm nvidia-304xx -y
 elif (($TMP==3))
-then pacman -S nvidia-340xx -y
+then pacman -S --noconfirm nvidia-340xx -y
 else echo Error ! Input the currect number !
 fi
 done
 elif (($VIDEO==4))
-then pacman -S xf86-video-ati -y
+then pacman -S --noconfirm xf86-video-ati -y
 else
 echo Error ! Input the number again
 fi
@@ -93,11 +93,11 @@ Server = https://mirrors.ustc.edu.cn/archlinuxcn/\$arch" >> /etc/pacman.conf
 TMP=n
 while [ "$TMP" == n ]
 do
-pacman -Syu&&pacman -S archlinuxcn-keyring&&pacman -S iw wpa_supplicant dialog networkmanager xorg-server xterm firefox yaourt wqy-zenhei wqy-microhei gnome-keyring
+pacman -Syu&&pacman -S --noconfirm archlinuxcn-keyring&&pacman -S --noconfirm networkmanager xorg-server firefox yaourt wqy-zenhei gnome-keyring
 systemctl enable NetworkManager
 read -p "Do you have bluetooth ? (y or Enter  " TMP
 if [ "$TMP" == y ]
-then pacman -S bluez blueman&&systemctl enable bluetooth
+then pacman -S --noconfirm bluez blueman&&systemctl enable bluetooth
 fi
 read -p "Successfully installed ? (n or Enter  " TMP
 done
@@ -120,25 +120,25 @@ echo "[1]  Gnome
 [10]  i3wm"
 read DESKTOP
 case $DESKTOP in
-    1) pacman -S gnome
+    1) pacman -S --noconfirm gnome
     ;;
-    2) pacman -S plasma kdebase kdeutils kdegraphics kde-l10n-zh_cn sddm
+    2) pacman -S --noconfirm plasma kdebase kdeutils kdegraphics kde-l10n-zh_cn sddm
     ;;
-    3) pacman -S lxde lightdm lightdm-gtk-greeter 
+    3) pacman -S --noconfirm lxde lightdm lightdm-gtk-greeter 
     ;;
-    4) pacman -S lxqt lightdm lightdm-gtk-greeter
+    4) pacman -S --noconfirm lxqt lightdm lightdm-gtk-greeter
     ;;
-    5) pacman -S mate mate-extra lightdm lightdm-gtk-greeter
+    5) pacman -S --noconfirm mate mate-extra lightdm lightdm-gtk-greeter
     ;;
-    6) pacman -S xfce4 xfce4-goodies lightdm lightdm-gtk-greeter
+    6) pacman -S --noconfirm xfce4 xfce4-goodies lightdm lightdm-gtk-greeter
     ;;
-    7) pacman -S deepin deepin-extra lightdm lightdm-gtk-greeter
+    7) pacman -S --noconfirm deepin deepin-extra lightdm lightdm-gtk-greeter
     ;;
-    8) pacman -S budgie-desktop lightdm lightdm-gtk-greeter
+    8) pacman -S--noconfirm  budgie-desktop lightdm lightdm-gtk-greeter
     ;;
-    9) pacman -S cinnamon lightdm lightdm-gtk-greeter
+    9) pacman -S --noconfirm cinnamon lightdm lightdm-gtk-greeter
     ;;
-    10) pacman -S i3 rofi rxvt-unicode lightdm lightdm-gtk-greeter
+    10) pacman -S --noconfirm i3 rofi rxvt-unicode lightdm lightdm-gtk-greeter
     ;;
     *) echo Error ! Input the number again
     ;;
